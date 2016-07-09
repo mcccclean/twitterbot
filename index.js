16824e4e34d709bf3facd3ed93e7ae6a16af6d3f
Tom McLean <tom.a.mclean@gmail.com>
2016-07-09 10:40:40 +1000

    Add poll function

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 var twitter = require('twitter');
 var wordfilter = require('wordfilter');
 var chalk = require('chalk');
-var config = require('config');
 var debug_log = require('pretty-good-log')('twitter');
 var emoji = require('node-emoji');
 
@@ -73,8 +72,8 @@ Twitbot.prototype.stream = function(s, callback, allow) {
                 }
             });
 
-            stream.on('error', function(error) {
-                debug_log('stream error', chalk.red(error));
+            stream.on('error', function(err) {
+                debug_log('stream error', chalk.red(err));
                 stream.destroy();
             });
 
@@ -91,9 +90,9 @@ Twitbot.prototype.search = function(term, allow) {
             q: term, 
             result_type: 'recent',
             count: 100 
-        }, function(error, tweets, response) {
-            if(error) {
-                reject(error);
+        }, function(err, tweets, response) {
+            if(err) {
+                reject(err);
             } else {
                 var filtered = tweets.statuses.filter(function(t) {
                     return me.isTweetSuitable(t, allow);
@@ -104,17 +103,70 @@ Twitbot.prototype.search = function(term, allow) {
     });
 };
 
-Twitbot.prototype.profile = function(description) {
+function wait(ms, pass) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve(pass);   
+        }, ms);
+    }); 
+}
+
+Twitbot.prototype.poll = function(endpoint, data, callback) {
+    var me = this;
+    var since = (data || {}).since_id || 0;
+    return this.get(endpoint, data).then(function(tweets) {
+        var newsince = since;
+        tweets.map(function(t) {
+            if(t.id > newsince) {
+                newsince = t.id;
+            }
+            if(t.id > since) {
+                callback(t);
+            }
+        });
+        return newsince;
+    }).then(function(max_id) {
+        return wait(600000, max_id);
+    }).then(function(max_id) {
+        var newdata = Object.assign({}, data || {}, { since_id: max_id });
+        return me.poll(endpoint, newdata, callback);
+    });
+};
+
+Twitbot.prototype.post = function(endpoint, data) {
     var client = this.client;
     return new Promise(function(resolve, reject) {
-        client.post('account/update_profile', { description: description }, function(err, response) {
+        client.post(endpoint, data || {}, function(err, response) {
             if(err) {
+                debug_log('post error', chalk.red(JSON.stringify(err)));
                 reject(err);
             } else {
                 resolve(response);
             }
         });
     });
+};
+
+Twitbot.prototype.get = function(endpoint, data) {
+    var client = this.client;
+    return new Promise(function(resolve, reject) {
+        client.get(endpoint, data || {}, function(err, tweets, response) {
+            if(err) {
+                debug_log('get error', chalk.red(JSON.stringify(err)));
+                reject(err);
+            } else {
+                resolve(tweets);
+            }
+        });
+    });
+};
+
+Twitbot.prototype.profile = function(description) {
+    return this.post('account/update_profile', { description: description });
+};
+
+Twitbot.prototype.retweet = function(tweet) {
+    this.post('statuses/retweet/' + tweet.id, {});
 };
 
 Twitbot.prototype.tweet = function(s) {
